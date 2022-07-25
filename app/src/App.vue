3ead64ff0b494e8b334ede90e5eaf6392848aeca
franco ledesma <ledesma420f@gmail.com>
2022-07-25 20:19:24 -0300

    formulario

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,128 +1,38 @@
 <template>
   <div id="app">
-    <div v-if="!canAccess">
-      <ul class="nav nav-pills nav-justified mb-3">
-        <li class="nav-item">
-          <a class="navbar-toggler active" data-mdb-toggle="pill" @click="cambiardeRutaLoginPage" role="tab" aria-controls="pills-login" aria-selected="true">Login</a>
-        </li>
-        <li class="nav-item">
-          <a class="navbar-toggler" data-mdb-toggle="pill" @click="cambiardeRutaLoginPage" role="tab" aria-controls="pills-register" aria-selected="false">Register</a>
-        </li>
-      </ul>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Formulario Clase 8"/>
+    <FormularioWeb @emit-agregar-usuario="addUsuarioTabla"/>
+    <TablaWeb :usuarios="usuarios"/>
 
-      <LoginPage
-      v-show="estoyEnLogin"
-      @changeFlag="recibiElMensaje" 
-      :usuarios="listadoDeUsuarios"/>
-    
-      <Registro 
-      v-show="!estoyEnLogin"
-      @enviarRegistro="recibirRegistro" 
-      />
-    </div>
-    
-    <div v-else>
-    <MainPage 
-    v-if="canAccess" 
-    @changeFlagFromMain="recibiElMensaje" 
-     @emitVerDetalle="recibirVerDetalle"
-    :productos="listadoDeProductos"
-    @emitAgregarAlCarrito="recibirAgregarAlCarrito"
-    />
-    
-    <Carrito :carro='carrito' @emitActualizarCarritoPrincipal="recibirActualizarAlCarrito"/>
-    
-    <Detalle v-if="selected" :producto="selected"/>
-
-    </div>
-</div>
+  </div>
 </template>
 
 <script>
-import LoginPage from './components/LoginPage.vue'
-import MainPage from './components/MainPage.vue'
-import Registro from './components/Registro.vue'
-import Carrito from './components/Carrito.vue'
-import Detalle from './components/Detalle.vue'
+import HelloWorld from './components/HelloWorld.vue'
+import FormularioWeb from './components/FormularioWeb.vue'
+import TablaWeb from './components/TablaWeb.vue'
 export default {
   name: 'App',
   components: {
-    LoginPage,
-    MainPage,
-    Registro,
-    Carrito,
-    Detalle
+    HelloWorld, FormularioWeb,TablaWeb
   },
-  data(){
-    return{
-    canAccess:false,
-    estoyEnLogin:true,
-    listadoDeUsuarios:[],
-    listadoDeProductos:[{
-  "id": 1,
-  "descripcion": "Arlistan",
-  "titulo": "Cafe",
-  "cantidad": "10",
-  "precio": "200",
-  "imagen": "https://robohash.org/deseruntsolutanon.png?size=50x50&set=set1"
-}, {
-  "id": 2,
-  "descripcion": "Pro-Plan",
-  "titulo": "Alimento de Gato",
-  "cantidad": "50",
-  "precio": "1200",
-  "imagen": "https://robohash.org/sequianimimagni.png?size=50x50&set=set1"
-}, {
-  "id": 3,
-  "descripcion": "Sin Gas",
-  "titulo": "Agua",
-  "cantidad": "200",
-  "precio": "100",
-  "imagen": "https://robohash.org/ipsamveritatisimpedit.png?size=50x50&set=set1"
-}, {
-  "id": 4,
-  "descripcion": "Blanco",
-  "titulo": "Vino",
-  "cantidad": "300",
-  "precio": "500",
-  "imagen": "https://robohash.org/dolorumtotamaut.png?size=50x50&set=set1"
-}, {
-  "id": 5,
-  "descripcion": "Surtido",
-  "titulo": "Galletitas",
-  "cantidad": "30",
-  "precio": "200",
-  "imagen": "https://robohash.org/autnihilquisquam.png?size=50x50&set=set1"
-}],
-    carrito:[],
-    selected: null
+  data() {
+        return {
+            usuarios: []
+        }
+    },
+    methods: {
+        addUsuarioTabla(usuario) {
+            console.log(usuario);
+            const nuevoUsuario = {...usuario};
+            this.usuarios.push(nuevoUsuario);
+        }
     }
-  },
-  methods:{
-    recibiElMensaje(){
-      this.canAccess = !this.canAccess
-    },
-    recibirRegistro(payload){
-      this.listadoDeUsuarios.push(payload)
-    },
-    recibirAgregarAlCarrito(payload){
-      this.carrito.push(payload)
-    },
-    recibirActualizarAlCarrito(payload){
-          //this.carrito = [...payload]
-          this.carrito = Object.assing(this.carrito,payload)
-    },
-    recibirVerDetalle(payload){
-      this.selected = payload
-    },
-    cambiardeRutaLoginPage(){
-      this.estoyEnLoginPage = !this.estoyEnLoginPage;
-    }
-  }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
