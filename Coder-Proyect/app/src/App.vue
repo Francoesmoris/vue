d094872b096bce51046629b02b451d0110057b0d
franco ledesma <ledesma420f@gmail.com>
2022-08-15 15:02:00 -0300

    app

---
+++ Coder-Proyect/app/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+
+<div class="container">
+  <h1>Carrito</h1>
+</div>
+  
+  <Card />
+  
+</template>
+
+<script>
+import Card from './components/Card'
+
+export default {
+  name: 'App',
+  components: {
+    Card
+    
+  }
+}
+</script>
+
+
